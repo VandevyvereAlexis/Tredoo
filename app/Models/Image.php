0b6673e87e24fc2aa843677e5d14aa99d964603c
VandevyvereAlexis <131187655+VandevyvereAlexis@users.noreply.github.com>
2024-12-20 15:06:38 +0100

    Mise en place ImageSeeder et ImageFactory

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['annonce_id', 'url', 'position'];
 }
