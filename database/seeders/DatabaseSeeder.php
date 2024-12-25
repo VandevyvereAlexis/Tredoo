445e3f9f1e4dec9efde8bb8f81f8a75c19982440
VandevyvereAlexis <131187655+VandevyvereAlexis@users.noreply.github.com>
2024-12-25 22:46:05 +0100

    Configuration du DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            RoleSeeder::class,
+            BrandSeeder::class,
+            CarModelSeeder::class,
+            UserSeeder::class,
+            AnnonceSeeder::class,
+            ImageSeeder::class,
+            ConversationSeeder::class,
+            FavoriteSeeder::class,
+            MessageSeeder::class,
+            ConversationStateSeeder::class,
         ]);
     }
 }
