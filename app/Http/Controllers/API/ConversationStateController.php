716baf5424d3a39176fe209d80563a633dfc18c0
VandevyvereAlexis <131187655+VandevyvereAlexis@users.noreply.github.com>
2025-01-10 11:21:50 +0100

    Création des controllers et des routes

---
+++ app/Http/Controllers/API/ConversationStateController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\ConversationState;
+use Illuminate\Http\Request;
+
+class ConversationStateController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ConversationState $conversationState)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ConversationState $conversationState)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ConversationState $conversationState)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,28 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\API\AnnonceController;
+use App\Http\Controllers\API\BrandController;
+use App\Http\Controllers\API\CarModelController;
+use App\Http\Controllers\API\ConversationController;
+use App\Http\Controllers\API\ConversationStateController;
+use App\Http\Controllers\API\FavoriteController;
+use App\Http\Controllers\API\ImageController;
+use App\Http\Controllers\API\MessageController;
+use App\Http\Controllers\API\RoleController;
+use App\Http\Controllers\API\UserController;
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('annonces', AnnonceController::class);
+Route::apiResource('brands', BrandController::class);
+Route::apiResource('carmodels', CarModelController::class);
+Route::apiResource('conversations', ConversationController::class);
+Route::apiResource('conversationstates', ConversationStateController::class);
+Route::apiResource('favorites', FavoriteController::class);
+Route::apiResource('images', ImageController::class);
+Route::apiResource('messages', MessageController::class);
+Route::apiResource('roles', RoleController::class);
+Route::apiResource('users', UserController::class);
